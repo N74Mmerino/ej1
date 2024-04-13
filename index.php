90737aae29a6b1861014d7a0daa52f81561789a2
N74Mmerino <nnicolasmmerino@gmail.com>
2024-04-12 21:49:48 -0300

    ejercicio1

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$a1=10;
+$a2=5;
+$a3=$a1/$a2;
+
+echo ' a1 = '.$a1;
+echo "<br>";
+echo ' a2 = '.$a2;
+echo "<br>";
+echo ' a3 = ' .$a3;
+echo "<br>";
+echo ' ACLARACION:La variable a3 es el resultado de la division entre a1 y a2 ';
